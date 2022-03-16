cbaa2be4afa6efcda31cf0fb24aa3b94e58ea887
hks <a113879@163.com>
2022-03-16 23:22:46 +0800

    upload

--- a/project_1/project_1.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\XLINX\\Vivado\\2015.4\\bin
-call %xv_path%/xsim a_behav -key {Behavioral:sim_1:Functional:a} -tclbatch a.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
